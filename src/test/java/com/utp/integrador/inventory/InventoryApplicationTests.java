5b582aae46623c124474bbe1b5ffef4b069252da
Luis Osis <luis_14xiv@hotmail.com>
2020-06-01 03:02:49 -0500

    build project

---
+++ src/test/java/com/utp/integrador/inventory/InventoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.utp.integrador.inventory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InventoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
